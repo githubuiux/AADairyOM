6fe9afe3f8921db68e60ecfcad52fc57f0416141
githubuiux <uiuxista@gmail.com>
2023-04-17 20:27:38 +0300

    Initial Commit

---
+++ AADairyOM/AADairyOMApp.swift
@@ -0,0 +1,17 @@
+//
+//  AADairyOMApp.swift
+//  AADairyOM
+//
+//  Created by Levent on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AADairyOMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
